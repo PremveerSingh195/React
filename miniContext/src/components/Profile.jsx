0add463f8e93d9ecac7864a150ec91ce1d7068e9
PremveerSingh195 <prem.veer2836@gmail.com>
2023-12-08 15:07:22 +0530

    mini

--- miniContext/src/components/Profile.jsx
+++ miniContext/src/components/Profile.jsx
@@ -7,7 +7,7 @@ function Profile() {
     const {user} = useContext(UserContext)
 
     if(!user) return <p>Please login</p>
-    return <p>welcome : {user.username}</p>
+    return <p>welcome : {user.username} ,Your password :{user.password}</p>
 }
 
 export default Profile
